bf9f23f22f8ec42a0fc03eb6bddb54260609e834
Vladislav <lycakvladislav@gmail.com>
2023-12-07 07:13:56 -0500

    add error handling to job and applicant controllers

--- controller/applicant.controller.js
+++ controller/applicant.controller.js
@@ -2,44 +2,71 @@ const db = require('../db');
 
 class ApplicantController {
   async createApplicant(req, res) {
-    const { applicant_name, resume, email } = req.body;
-    console.log(applicant_name, resume, email);
-    const newApplicant = await db.query(
-      'INSERT INTO applicant (applicant_name, resume, email) values ($1, $2, $3) RETURNING *',
-      [applicant_name, resume, email]
-    );
-    res.send(newApplicant.rows[0]);
+    try {
+      const { applicant_name, resume, email } = req.body;
+      console.log(applicant_name, resume, email);
+      const newApplicant = await db.query(
+        'INSERT INTO applicant (applicant_name, resume, email) values ($1, $2, $3) RETURNING *',
+        [applicant_name, resume, email]
+      );
+      res.send(newApplicant.rows[0]);
+    } catch (error) {
+      console.error('Error creating the applicant:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async getAllApplicant(req, res) {
-    const applicants = await db.query('SELECT * FROM applicant');
-    res.json(applicants.rows);
+    try {
+      const applicants = await db.query('SELECT * FROM applicant');
+      res.json(applicants.rows);
+    } catch (error) {
+      console.error('Error getting all applicants:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async getApplicant(req, res) {
-    const id = req.params.id;
-    const applicant = await db.query('SELECT * FROM applicant where id = $1', [
-      id,
-    ]);
-    res.json(applicant.rows[0]);
+    try {
+      const id = req.params.id;
+      const applicant = await db.query(
+        'SELECT * FROM applicant where id = $1',
+        [id]
+      );
+      res.json(applicant.rows[0]);
+    } catch (error) {
+      console.error('Error getting applicant by ID:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async updateApplicant(req, res) {
-    const { applicant_name, resume, email, id } = req.body;
-    const applicant = await db.query(
-      'UPDATE applicant set applicant_name = $1,  resume = $2, email = $3 where id = $4 RETURNING *',
-      [applicant_name, resume, email, id]
-    );
-    res.json(applicant.rows[0]);
+    try {
+      const { applicant_name, resume, email, id } = req.body;
+      const applicant = await db.query(
+        'UPDATE applicant set applicant_name = $1,  resume = $2, email = $3 where id = $4 RETURNING *',
+        [applicant_name, resume, email, id]
+      );
+      res.json(applicant.rows[0]);
+    } catch (error) {
+      console.error('Error updating the applicant:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async deleteApplicant(req, res) {
-    const id = req.params.id;
-    console.log(id);
-    const applicant = await db.query('DELETE FROM applicant where id = $1', [
-      id,
-    ]);
-    res.json(applicant.rows);
+    try {
+      const id = req.params.id;
+      console.log(id);
+      const applicant = await db.query(
+        'DELETE FROM applicant where id = $1 RETURNING *',
+        [id]
+      );
+      res.json(applicant.rows[0]);
+    } catch (error) {
+      console.error('Error deleting the applicant:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
--- controller/job.controller.js
+++ controller/job.controller.js
@@ -2,40 +2,67 @@ const db = require('../db');
 
 class JobController {
   async createJob(req, res) {
-    const { title, content, companyId } = req.body;
-    const newJob = await db.query(
-      'INSERT INTO job (title, content, company_id) values ($1, $2, $3) RETURNING *',
-      [title, content, companyId]
-    );
-    res.json(newJob.rows[0]);
+    try {
+      const { title, content, companyId } = req.body;
+      const newJob = await db.query(
+        'INSERT INTO job (title, content, company_id) values ($1, $2, $3) RETURNING *',
+        [title, content, companyId]
+      );
+      res.json(newJob.rows[0]);
+    } catch (error) {
+      console.error('Error creating the job:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async getJobsByCompany(req, res) {
-    const id = req.params.id;
-    const jobs = await db.query('select * from job where company_id = $1', [
-      id,
-    ]);
-    res.json(jobs.rows);
+    try {
+      const id = req.params.id;
+      const jobs = await db.query('select * from job where company_id = $1', [
+        id,
+      ]);
+      res.json(jobs.rows);
+    } catch (error) {
+      console.error('Error getting jobs by company:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async getAllJobs(req, res) {
-    const jobs = await db.query('SELECT * FROM job');
-    res.json(jobs.rows);
+    try {
+      const jobs = await db.query('SELECT * FROM job');
+      res.json(jobs.rows);
+    } catch (error) {
+      console.error('Error getting all jobs:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async updateJob(req, res) {
-    const { title, content, id } = req.body;
-    const job = await db.query(
-      'UPDATE job set title = $1, content = $2 where id = $3 RETURNING *',
-      [title, content, id]
-    );
-    res.json(job.rows[0]);
+    try {
+      const { title, content, id } = req.body;
+      const job = await db.query(
+        'UPDATE job set title = $1, content = $2 where id = $3 RETURNING *',
+        [title, content, id]
+      );
+      res.json(job.rows[0]);
+    } catch (error) {
+      console.error('Error updating the job:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async deleteJob(req, res) {
-    const id = req.params.id;
-    const jobs = await db.query('DELETE FROM job where id = $1', [id]);
-    res.json(jobs.rows);
+    try {
+      const id = req.params.id;
+      const jobs = await db.query('DELETE FROM job where id = $1 RETURNING *', [
+        id,
+      ]);
+      res.json(jobs.rows[0]);
+    } catch (error) {
+      console.error('Error deleting the job:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
