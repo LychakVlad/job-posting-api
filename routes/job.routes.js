a4e8be51dced682842fb5e77708e85e1b8dc0062
Vladislav <lycakvladislav@gmail.com>
2024-01-08 21:40:58 -0500

    add auth middleware

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -1,10 +1,11 @@
 const Router = require('express');
 const router = new Router();
 const jobController = require('../controller/job.controller');
+const authorize = require('../middleware/auth');
 
 router.post('/job', jobController.createJob);
 router.get('/job/:id', jobController.getJobsByCompany);
-router.get('/job', jobController.getAllJobs);
+router.get('/job', authorize(), jobController.getAllJobs);
 router.put('/job', jobController.updateJob);
 router.delete('/job/:id', jobController.deleteJob);
 
