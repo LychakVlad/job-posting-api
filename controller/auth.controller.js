a0ed0ccbf2802467c424c982eff842a5f4ae7d21
Vladislav <lycakvladislav@gmail.com>
2024-01-09 20:36:48 -0500

    add sign up route

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,17 +1,32 @@
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
 class AuthController {
   async registration(req, res) {
     try {
       const { username, role, password, email } = req.body;
       const created_on = new Date();
+
+      const candidate = await db.query(
+        'SELECT username  FROM users where username = $1 ',
+        [username]
+      );
+
+      if (candidate.rows.length !== 0) {
+        console.log(candidate.rows.length);
+        return res.status(400).json({ error: 'User already exists!' });
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 7);
+
       const newUser = await db.query(
         'INSERT INTO users (username, role, password, email, created_on) values ($1, $2, $3, $4, $5) RETURNING *',
-        [username, role, password, email, created_on]
+        [username, role, hashPassword, email, created_on]
       );
-      res.send(newUser.rows[0]);
+
+      res.json({ message: 'User registered successfully!' });
     } catch (error) {
-      console.error('Error creating the applicant:', error);
+      console.error('Error creating the user:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }
