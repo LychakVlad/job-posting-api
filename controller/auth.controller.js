76db40c2fa58cb7bc8ea3b1dac7bf11b119909fd
Vladislav <lycakvladislav@gmail.com>
2024-01-09 21:10:35 -0500

    add sign in route

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,17 @@
 const { validationResult } = require('express-validator');
 const db = require('../db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config');
+
+const generateAccessToken = (id, role) => {
+  const payload = {
+    id,
+    role,
+  };
+
+  return jwt.sign(payload, secret, { expiresIn: '24h' });
+};
 
 class AuthController {
   async registration(req, res) {
@@ -20,7 +31,6 @@ class AuthController {
       );
 
       if (candidate.rows.length !== 0) {
-        console.log(candidate.rows.length);
         return res.status(400).json({ error: 'User already exists!' });
       }
 
@@ -40,13 +50,41 @@ class AuthController {
 
   async login(req, res) {
     try {
-    } catch (error) {}
+      const { username, password, email } = req.body;
+
+      const user = await db.query('SELECT *  FROM users where username = $1 ', [
+        username,
+      ]);
+
+      if (user.rows.length === 0) {
+        return res.status(400).json({ error: 'User is not found!' });
+      }
+
+      const validPassword = bcrypt.compareSync(password, user.rows[0].password);
+      if (!validPassword) {
+        return res.status(400).json({ error: 'Password is not correct!' });
+      }
+
+      const token = generateAccessToken(
+        user.rows[0].user_id,
+        user.rows[0].role
+      );
+
+      return res.json({ token });
+    } catch (error) {
+      console.error('Error loggin in:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 
   async getUsers(req, res) {
     try {
-      res.json(`everything is OK ${req}`);
-    } catch (error) {}
+      const users = await db.query('SELECT *  FROM users ');
+      res.json(users.rows);
+    } catch (error) {
+      console.error('Error getting the users:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
